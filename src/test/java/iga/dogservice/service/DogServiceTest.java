e6b6e08900d22918f8e511ca4660e0f6fb0280d4
AjgaK <s24646@pjwstk.edu.pl>
2026-08-14 20:28:33 +0200

    Added tests

---
+++ src/test/java/iga/dogservice/service/DogServiceTest.java
@@ -0,0 +1,37 @@
+package iga.dogservice.service;
+
+import iga.dogservice.model.Dog;
+import iga.dogservice.repository.DogRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DogServiceTest {
+    @Mock
+    private DogRepository dogRepository;
+
+    @InjectMocks
+    private DogService dogService;
+
+    @Test
+    void shouldReturnDogById() {
+        Dog dog = new Dog(
+                1L,
+                "Baki",
+                "Shiba",
+                9,
+                true
+        );
+        when(dogRepository.findById(1L)).thenReturn(Optional.of(dog));
+        Dog result = dogService.getDogById(1L);
+        Assertions.assertEquals("Baki", result.getName());
+    }
+}
