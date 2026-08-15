6b13273e79a87319fb380345f8e8dc3ff46ce3ae
AjgaK <s24646@pjwstk.edu.pl>
2026-08-15 18:30:20 +0200

    Deleted unused imports

--- src/test/java/iga/dogservice/service/DogServiceTest.java
+++ src/test/java/iga/dogservice/service/DogServiceTest.java
@@ -3,7 +3,6 @@ package iga.dogservice.service;
 import iga.dogservice.exception.DogNotFoundException;
 import iga.dogservice.model.Dog;
 import iga.dogservice.repository.DogRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
