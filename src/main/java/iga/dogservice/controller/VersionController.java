d933303fcd1f08cf08fdc5c53e1ccf478183496e
AjgaK <s24646@pjwstk.edu.pl>
2026-08-16 22:25:58 +0200

    Added versioning

---
+++ src/main/java/iga/dogservice/controller/VersionController.java
@@ -0,0 +1,23 @@
+package iga.dogservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/version")
+public class VersionController {
+    @Value("${app.version}")
+    private String version;
+
+    @GetMapping
+    public Map<String, String> getVersion() {
+        return Map.of(
+                "service", "dog-service",
+                "version", version
+        );
+    }
+}
