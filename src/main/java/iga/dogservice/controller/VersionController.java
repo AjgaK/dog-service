67e6ad2caff1bb1713fd634f6abc11643ed5445d
AjgaK <s24646@pjwstk.edu.pl>
2026-08-24 12:42:58 +0200

    Push-based deployment test: Third run

--- src/main/java/iga/dogservice/controller/VersionController.java
+++ src/main/java/iga/dogservice/controller/VersionController.java
@@ -18,7 +18,7 @@ public class VersionController {
         return Map.of(
                 "service", "dog-service",
                 "version", version,
-                "test", "push deployment test second run"
+                "test", "push deployment test third run"
         );
     }
 }
